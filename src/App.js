2f33a7c9196c0aaac86d5fec6a5ef0659978bcee
yandeqiang <yandeqiang@didichuxing.com>
2018-11-15 22:23:20 +0800

    test index.html‘

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src="./assets/images/banner.jpg" alt=""/>
+      </div>
+    );
+  }
+}
+
+export default App;
