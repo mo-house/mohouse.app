30188fb997348e3454d745e3ee943e12f19a429a
yandeqiang <yandeqiang@didichuxing.com>
2018-11-19 19:37:35 +0800

    push build files

---
+++ build/precache-manifest.199dd7bd759961b7be138a5133605e29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a5abf80d3dc234d574c",
+    "url": "/static/js/main.6a5abf80.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "6a5abf80d3dc234d574c",
+    "url": "/static/css/main.6bea6e3d.chunk.css"
+  },
+  {
+    "revision": "bec8f4f6d6a9db75be56cd832cd580be",
+    "url": "/index.html"
+  }
+];
